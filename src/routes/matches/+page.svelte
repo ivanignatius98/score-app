2dabd6f754ff131f34fb666eba061379eb4646af
Ivan Handoko <ivanignatius98@gmail.com>
2023-11-26 21:59:00 +0700

    init list

--- src/routes/matches/+page.svelte
+++ src/routes/matches/+page.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
 	export let data: any;
-	console.log(data);
 	$: ({ record } = data);
 </script>
 
-<h1>Select a car</h1>
-<ul>
-	{#each record as car}
-		<li>
-			<h3>{car._id}</h3>
-			<h3>{car.name}</h3>
-		</li>
-	{/each}
-</ul>
+<div class="flow-root mt-6">
+	<ul role="list" class="-my-5 divide-y divide-gray-200">
+		{#each record as person}
+			<li class="py-4">
+				<div class="flex items-center space-x-4">
+					<div class="flex-shrink-0">
+						<img class="h-8 w-8 rounded-full" src={person.imageUrl} alt="" />
+					</div>
+					<div class="flex-1 min-w-0">
+						<p class="text-sm font-medium text-white truncate">{person.name}</p>
+						<p class="text-sm text-gray-500 truncate">{'@' + person.handle}</p>
+					</div>
+					<div>
+						<span
+							class='bg-gray-900 group-hover:bg-gray-800 ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full'
+						>
+							View
+						</span>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</div>
+<div class="mt-6">
+	<a
+		href="#"
+		class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+	>
+		View all
+	</a>
+</div>
