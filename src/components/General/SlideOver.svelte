db8ddac4795512a81f74e3fb1124c51f917fc4b7
Ivan Handoko <ivanignatius98@gmail.com>
2023-11-27 00:11:22 +0700

    add list stylings and slideover component

---
+++ src/components/General/SlideOver.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { fly, fade } from 'svelte/transition';
+	export let showSidePanel: boolean;
+	let width;
+</script>
+
+{#if showSidePanel}
+	<aside class="relative z-10">
+		<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" transition:fade />
+		<div class="fixed inset-0 overflow-hidden">
+			<div class="absolute inset-0 overflow-hidden">
+				<div class="absolute inset-y-0 right-0 flex max-w-full pl-10">
+					<div class="my-auto h-full w-screen max-w-2xl">
+						<div
+							bind:clientWidth={width}
+							transition:fly={{ x: width }}
+							class="m-auto flex h-full flex-col overflow-y-scroll bg-gray-900 py-6 shadow-xl"
+						>
+							<div class="px-4 sm:px-6">
+								<div class="flex items-start justify-between">
+									<h2 class="text-lg font-medium text-white">Panel title</h2>
+									<div class="ml-3 flex h-7 items-center">
+										<button
+											class="rounded-md bg-gray-900 font-bold text-gray-400 hover:text-gray-500 w-6 h-6 relative"
+											on:click={() => (showSidePanel = false)}
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												aria-hidden="true"
+												class="oc se"
+												><path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M6 18L18 6M6 6l12 12"
+												></path></svg
+											>
+										</button>
+									</div>
+								</div>
+							</div>
+							<div class="relative mt-6 flex-1 px-4 sm:px-6">
+								<div class="absolute inset-0 px-4 sm:px-6">
+									<div class="h-full border-2 border-dashed border-gray-200" aria-hidden="true">
+										Some content
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</aside>
+{/if}
--- src/routes/matches/+page.svelte
+++ src/routes/matches/+page.svelte
@@ -1,37 +1,53 @@
 <script lang="ts">
+	import SlideOver from '../../components/General/SlideOver.svelte';
+
 	export let data: any;
 	$: ({ record } = data);
+	let showSidePanel = false;
 </script>
 
 <div class="flow-root mt-6">
-	<ul role="list" class="-my-5 divide-y divide-gray-200">
+	<ul role="list" class="-my-5 divide-y divide-gray-800">
 		{#each record as person}
 			<li class="py-4">
 				<div class="flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img class="h-8 w-8 rounded-full" src={person.imageUrl} alt="" />
-					</div>
 					<div class="flex-1 min-w-0">
-						<p class="text-sm font-medium text-white truncate">{person.name}</p>
-						<p class="text-sm text-gray-500 truncate">{'@' + person.handle}</p>
+						<p class="text-sm font-medium text-white">
+							{person.name}
+							<span
+								class="bg-green-500 bg-opacity-10 text-green-500 text-opacity-100 ring-1 ring-green-500 ring-opacity-20 ring-inset ml-3 inline-block py-1 px-2 text-xs font-medium rounded-md"
+							>
+								Complete
+							</span>
+						</p>
+						<p class="text-xs text-gray-500 truncate mt-2">Created on March 17, 2023</p>
 					</div>
-					<div>
-						<span
-							class='bg-gray-900 group-hover:bg-gray-800 ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full'
-						>
-							View
-						</span>
-					</div>
+					<svg
+						class="flex-shrink-0 w-1 h-2"
+						viewBox="0 0 24 44"
+						preserveAspectRatio="none"
+						fill="none"
+						stroke="currentColor"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+					>
+						<path stroke-width="2" d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+					</svg>
 				</div>
 			</li>
 		{/each}
 	</ul>
 </div>
 <div class="mt-6">
-	<a
-		href="#"
-		class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+	<button
+		type="button"
+		class="w-full flex justify-center items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md text-white bg-white bg-opacity-10 hover:bg-opacity-20"
 	>
 		View all
-	</a>
+	</button>
 </div>
+<div>
+	<button class="p-3" on:click={() => (showSidePanel = true)}> Show side panel </button>
+</div>
+
+<SlideOver bind:showSidePanel></SlideOver>
