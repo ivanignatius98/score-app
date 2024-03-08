4c012143ca770ff088ccdbc2cd67a5a083a8adef
Ivan Handoko <ivanignatius98@gmail.com>
2024-03-09 00:44:46 +0700

    WIP revamp scoring init court selection

---
+++ src/components/General/LandscapeOnly.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let landscapeMode = false;
+	let showPrompt = false;
+
+	onMount(() => {
+		const handleOrientation = () => {
+			showPrompt = window.innerWidth < 512 && window.innerWidth < window.innerHeight;
+			console.log(window.innerWidth);
+			// showPrompt = !landscapeMode && isMobile();
+		};
+
+		// const isMobile = () => {
+		//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+		// };
+
+		window.addEventListener('resize', handleOrientation);
+		handleOrientation();
+
+		return () => {
+			window.removeEventListener('resize', handleOrientation);
+		};
+	});
+</script>
+
+{#if showPrompt}
+	<div class="landscape-prompt">
+		<div>
+			<h2>Please rotate your device to landscape mode</h2>
+		</div>
+	</div>
+{:else}
+	<slot />
+{/if}
+
+<style>
+	.landscape-prompt {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.8);
+		color: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 9999;
+	}
+</style>
--- src/models/Stat.ts
+++ src/models/Stat.ts
@@ -1,29 +1,48 @@
 import mongoose, { Schema } from 'mongoose';
 
-const StatSchema = new mongoose.Schema({
-  match_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Matches'
-  },
-  player: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  type: {
-    type: String,
-    enum: ['FG', '3PT', 'BLK', 'REB', 'STL'],
-    default: 'FG'
-  },
-  coords: {
-    x: Number,
-    y: Number
-  },
-  made: {
-    type: Boolean,
-    default: false
-  },
-}, {
-  timestamps: true
-});
+const StatSchema = new mongoose.Schema(
+	{
+		match_id: {
+			type: Schema.Types.ObjectId,
+			ref: 'Matches'
+		},
+		player: {
+			type: Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		type: {
+			type: String,
+			enum: ['FG', '3PT', 'BLK', 'REB', 'STL'],
+			default: 'FG'
+		},
+		zone: {
+			type: String,
+			enum: [
+				'RIGHT-ELBOW',
+				'LEFT-ELBOW',
+				'FREE-THROW',
+				'LEFT-CORNER',
+				'RIGHT-CORNER',
+				'SHORT-LEFT-CORNER',
+				'SHORT-RIGHT-CORNER',
+				'LEFT-LOW-POST',
+				'RIGHT-LOW-POST',
+				'LEFT-WING',
+				'RIGHT-WING',
+				'POINT',
+				'HIGH-POST',
+				'PAINT'
+			],
+			default: null
+		},
+		made: {
+			type: Boolean,
+			default: false
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
 export const Stat = mongoose.model('Stat', StatSchema);
