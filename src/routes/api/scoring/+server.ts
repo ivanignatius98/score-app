62d07a89370c9799fd7431960b696931d2705208
Ivan Handoko <ivanignatius98@gmail.com>
2024-03-10 21:17:33 +0700

    move api back to root

--- src/routes/api/scoring/+server.ts
+++ src/routes/api/scoring/+server.ts
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
-import { Match } from "../../../../models/Match.js";
-import { Stat } from "../../../../models/Stat.js";
+import { Match } from '../../../models/Match.js';
+import { Stat } from '../../../models/Stat.js';
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	const { match_id, action, person } = await request.json();
@@ -9,9 +9,10 @@ export async function POST({ request }) {
 		match_id,
 		player: person._id,
 		type: action.type,
-		made: action.made
-	}
-	const newRecord = await Stat.create(populatePayload)
+		made: action.made,
+		zone: action.zone
+	};
+	const newRecord = await Stat.create(populatePayload);
 
 	return json({
 		success: true,
@@ -21,7 +22,7 @@ export async function POST({ request }) {
 
 export async function DELETE({ request }) {
 	const { _id } = await request.json();
-	await Stat.findOneAndDelete({ _id })
+	await Stat.findOneAndDelete({ _id });
 	return json({
 		success: true
 	});
--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,5 +1,5 @@
 import { json } from '@sveltejs/kit';
-import { User } from '../../../../models/User.js';
+import { User } from '../../../models/User.js';
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	const { name, groupId } = await request.json();
